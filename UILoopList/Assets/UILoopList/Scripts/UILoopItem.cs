86d78915ec5ec7eedc344380bae21f5994d8e389
yangqingyan <155246086@qq.com>
2016-06-24 16:19:45 +0800

    first

---
+++ UILoopList/Assets/UILoopList/Scripts/UILoopItem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class UILoopItem : MonoBehaviour {
+
+    [System.NonSerialized]
+    public int itemIndex;
+    [System.NonSerialized]
+    public GameObject itemObject;
+    private object data;
+	public void UpdateItem(int index,GameObject item)
+	{
+        itemIndex = index;
+        itemObject = item;
+	}
+    public virtual void Data(object data)
+	{
+        this.data = data;
+		transform.FindChild ("Text").GetComponent<Text> ().text = "数据" + data.ToString ();
+	}
+    public virtual object GetData()
+    {
+        return data;
+    }
+    public virtual void SetSelected(bool selected)
+    {
+
+    }
+}
---
+++ UILoopList/Assets/UILoopList/Scripts/UILoopListEzample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UILoopListEzample : MonoBehaviour {
+
+    public UILoopList uiList;
+	// Use this for initialization
+	void Start () {
+        uiList.onSelectedEvent = OnSelectedEventHandler;
+        List<int> listData = new List<int>();
+        for (int i = 0; i < 1000; i++)
+        {
+            listData.Add(i);
+        }
+        uiList.Data(listData);
+	}
+
+    private void OnSelectedEventHandler(UILoopItem item)
+    {
+        Debug.LogError("选择的单元数据为:" + item.GetData().ToString());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
